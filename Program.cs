6b757bc9ccb9fee6fc7bd7f008be9b9d1e99174d
Atsert2 <lia.jp2002@gmai.com>
2025-09-17 19:26:51 -0300

    Lista

---
+++ Program.cs
@@ -0,0 +1,3 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("----Minhas Frutas Preferidas---\n");Thread.Sleep(1000);
+Console.WriteLine("1.Goiaba\n2.Banana\n3.Morango\n4.Ameixa\n5.Melancia");
